c096e159aab1f2603617846e705ca86eb250e802
Stefan Op de Beek <ste7an@gmail.com>
2025-06-04 13:36:22 +0200

    script for merge firmware in single file + moved microblocks code

--- firmware_single_file/create_fiirmware.bat
+++ firmware_single_file/create_fiirmware.bat
@@ -1,3 +1,33 @@
-copy ..\.pio\build\esp32lvgl\*.bin .
+@echo off
+
+set "param=%1"
+if "%1"=="" (
+    set "param=esp32"
+)
+
+for /f %%a in ('wmic os get localdatetime ^| find "."') do set datetime=%%a
+
+REM Split into parts
+set "YYYY=%datetime:~0,4%"
+set "MM=%datetime:~4,2%"
+set "DD=%datetime:~6,2%"
+set "HH=%datetime:~8,2%"
+set "MN=%datetime:~10,2%"
+set "SS=%datetime:~12,2%"
+
+set "TIMESTAMP=%YYYY%%MM%%DD%-%HH%%MN%%SS%"
+
+REM Use timestamp in filename
+set "FILENAME=log_%TIMESTAMP%.txt"
+echo Creating file: %FILENAME%
+
+
+
+echo Parameter is: %param%
+copy ..\.pio\build\%param%\*.bin .
 copy ..\esp32\boot_app0.bin .
-esptool.exe --chip esp32 merge_bin  --flash_mode dio --flash_freq 80m --flash_size 4MB  0x1000 bootloader.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 firmware.bin -o firmware_MicroBlocks_LVGL_LMS_ESP32_20250601.bin
+esptool.exe --chip esp32 merge_bin  --flash_mode dio --flash_freq 80m --flash_size 4MB  0x1000 bootloader.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 firmware.bin -o ../lvgl_mb/firmware/firmware_MicroBlocks_LVGL_%param%_%TIMESTAMP%.bin
+del boot_app0.bin
+del bootloader.bin
+del partitions.bin
+del firmware.bin
