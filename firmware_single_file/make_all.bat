ede2e664e8dcc9f4ec3f33e50939ccfec4650843
Stefan Op de Beek <ste7an@gmail.com>
2025-09-09 07:38:33 +0200

    build firmware for v347

---
+++ firmware_single_file/make_all.bat
@@ -0,0 +1,8 @@
+call make_firmware lms-esp32_ili9341_lvgl v347
+call make_firmware lms-esp32_st7789_lvgl v347
+call make_firmware cydc-lvgl v347
+call make_firmware cydr-lvgl v347
+call make_firmware cydr-usb2-lvgl v347
+call make_firmware m5core2-lvgl v347
+call make_firmware esp32_breakout v347
+
