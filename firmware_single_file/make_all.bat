6e79a2341cfafdcdc06cc3d5ecd2af480e7cc2e7
Stefan Op de Beek <ste7an@gmail.com>
2025-09-10 22:48:09 +0200

    merge v348 and create make firmware batch file

--- firmware_single_file/make_all.bat
+++ firmware_single_file/make_all.bat
@@ -1,8 +1,9 @@
-call make_firmware lms-esp32_ili9341_lvgl v347
-call make_firmware lms-esp32_st7789_lvgl v347
-call make_firmware cydc-lvgl v347
-call make_firmware cydr-lvgl v347
-call make_firmware cydr-usb2-lvgl v347
-call make_firmware m5core2-lvgl v347
-call make_firmware esp32_breakout v347
+set "version=%1"
 
+call make_firmware lms-esp32_ili9341_lvgl %version%
+call make_firmware lms-esp32_st7789_lvgl %version%
+call make_firmware cydc-lvgl %version%
+call make_firmware cydr-lvgl  %version%
+call make_firmware cydr-usb2-lvgl  %version%
+call make_firmware m5core2-lvgl %version%
+call make_firmware esp32_breakout  %version%
---
+++ firmware_single_file/updatePrecompiled_lvgl.bat
@@ -0,0 +1,13 @@
+
+cd ..
+
+pio run -t buildfs -e lms-esp32_ili9341_lvgl
+pio run -t buildfs -e lms-esp32_st7789_lvgl
+pio run -t buildfs -e cydc-lvgl
+pio run -t buildfs -e cydr-lvgl
+pio run -t buildfs -e cydr-usb2-lvgl
+pio run -t buildfs -e m5core2-lvgl
+pio run -t buildfs -e iot-bus-lvgl
+
+cd firmware_single_file
+call make_all v348
